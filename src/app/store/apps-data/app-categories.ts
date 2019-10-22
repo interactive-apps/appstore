a6e1274912ad404c351ef9a1189baa26b338eba9
ibrahimwickama <ibrahimwickama@gmail.com>
2019-10-22 21:23:57 +0300

    feat: update items loading components

--- src/app/store/apps-data/app-categories.ts
+++ src/app/store/apps-data/app-categories.ts
@@ -1,11 +1,11 @@
 
 export const appCategories: any[] = [
   {categoryName: 'All', id: 'all', isSelected: true},
-  {categoryName: 'Web apps', id: 'web-app', isSelected: false},
-  {categoryName: 'Mobile apps', id: 'mobile-app', isSelected: false},
-  {categoryName: 'Desktop apps', id: 'desktop-app', isSelected: false},
-  {categoryName: 'Widgets', id: 'widget', isSelected: false},
+  {categoryName: 'Web apps', id: 'web-app', shortName: 'Web app', isSelected: false},
+  {categoryName: 'Mobile apps', id: 'mobile-app', shortName: 'Mobile app', isSelected: false},
+  {categoryName: 'Desktop apps', id: 'desktop-app', shortName: 'Desktop app', isSelected: false},
+  {categoryName: 'Widgets', id: 'widget', shortName: 'Widget', isSelected: false},
   // {categoryName: 'Social & fun', id: 'social-fun', isSelected: false},
   // {categoryName: 'Marketing', id: 'marketing', isSelected: false},
-  {categoryName: 'Developer tools', id: 'developer-tool', isSelected: false},
+  {categoryName: 'Developer tools', id: 'developer-tool', shortName: 'Developer tool', isSelected: false},
 ];
