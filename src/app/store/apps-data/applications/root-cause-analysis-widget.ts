776822cfbde72a935836a873fc358c080739a771
ibrahimwickama <ibrahimwickama@gmail.com>
2019-10-23 00:50:29 +0300

    feat: add icon images for apps

---
+++ src/app/store/apps-data/applications/root-cause-analysis-widget.ts
@@ -0,0 +1,16 @@
+
+export const rootCauseAnalysisWidget = {
+    appName: 'Root Cause Analysis Widget',
+    id: 'root-cause-widget',
+    categoryId: 'widget',
+    isTop3: true,
+    icon: './assets/app-icons/bna.png',
+    developer: {name: 'HISP-TZ', site: 'https://hisptanzania.org'},
+    features: [
+      {info: 'Feature one'}, {info: 'Feature two'},
+      {info: 'Feature three'}, {info: 'Feature four'},
+      {info: 'Feature five'}
+    ],
+    description: 'Vel risus commodo viverra maecenas accumsan lacus vel facilisis.' +
+    'Sagittis purus sit amet volutpat consequat mauris.'
+  };
